e2fb19c45ba33afdb985ece55eda681f8175ef31
parulsahu2006-ops <parulsahu2006@gmail.com>
2026-07-31 21:44:20 +0530

    Time: 1 ms (61.65%), Space: 45.1 MB (54.19%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int i=0;
+        int n=nums.length;
+        for(i=0;i<n-1;i++){
+            if((nums[i]%2)==(nums[i+1]%2)){
+                return false;
+            }
+            
+        }
+    return true;
+        
+    }
+}
