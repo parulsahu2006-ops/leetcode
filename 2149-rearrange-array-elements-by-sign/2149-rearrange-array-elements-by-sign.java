85a6bb337c68b4259b5b2ded5a7c46d5c749bec5
parulsahu2006-ops <parulsahu2006@gmail.com>
2026-09-06 23:42:28 +0530

    Time: 4 ms (40.78%), Space: 175.6 MB (42.85%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] rearrangeArray(int[] A) {
+
+        int n = A.length;
+
+        int[] ans = new int[n];
+
+        int posIndex = 0;
+        int negIndex = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (A[i] > 0) {
+                ans[posIndex] = A[i];
+                posIndex += 2;
+            } 
+            else {
+                ans[negIndex] = A[i];
+                negIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+}
